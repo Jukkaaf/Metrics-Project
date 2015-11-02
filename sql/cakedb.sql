f70ae35d7d898c0a8f056d4f67e5660936fe3276
Jukka <Jukka@192.168.1.46>
2015-11-02 21:05:20 +0200

    Database updated

--- sql/cakedb.sql
+++ sql/cakedb.sql
@@ -23,18 +23,33 @@ CREATE TABLE projects (
   status varchar(30),
   description varchar(100),
   is_public tinyint(1) NOT NULL,
-  importance tinyint(2),
   UNIQUE KEY (project_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE weeklyreports (
+  id int(10) NOT NULL auto_increment PRIMARY KEY,
+  project_id int(10) NOT NULL,
+  title varchar(50) NOT NULL,
+  week int(2) NOT NULL,
+  reqlink varchar(100),
+  problems varchar(400),
+  meetings varchar(400) NOT NULL,
+  additional varchar(400),
+  created_on date NOT NULL,
+  updated_on date,
+  FOREIGN KEY project_key (project_id) REFERENCES projects (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE metrics (
   id int(10) NOT NULL auto_increment PRIMARY KEY,
   project_id int(10) NOT NULL,
   metrictype_id int(10) NOT NULL,
+  weeklyreport_id int(10) NOT NULL,
   date date NOT NULL,
   value float NOT NULL,
   FOREIGN KEY project_key (project_id) REFERENCES projects (id),
-  FOREIGN KEY metrictype_key (metrictype_id) REFERENCES metrictypes (id)
+  FOREIGN KEY metrictype_key (metrictype_id) REFERENCES metrictypes (id),
+  FOREIGN KEY weeklyreport_key (weeklyreport_id) REFERENCES weeklyreports (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE members (
@@ -58,27 +73,11 @@ CREATE TABLE workinghours (
   FOREIGN KEY member_key (member_id) REFERENCES members (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE weeklyreports (
+CREATE TABLE weeklyhours (
   id int(10) NOT NULL auto_increment PRIMARY KEY,
-  project_id int(10) NOT NULL,
-  title varchar(50) NOT NULL,
-  date date NOT NULL,
-  reqlink varchar(100),
-  problems varchar(400),
-  meetings varchar(400) NOT NULL,
-  additional varchar(400),
-  FOREIGN KEY project_key (project_id) REFERENCES projects (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE requirements (
-  id int(10) NOT NULL,
-  changenum int(10) NOT NULL,
-  project_id int(10) NOT NULL,
-  name varchar(50),
-  description varchar(500) NOT NULL,
-  status int(2) NOT NULL,
-  version int(2) NOT NULL,
-  date date NOT NULL,
-  PRIMARY KEY id (id, changenum),
-  FOREIGN KEY project_key (project_id) REFERENCES projects (id)
+  weeklyreport_id int(10) NOT NULL,
+  member_id int(10) NOT NULL,
+  duration float NOT NULL,
+  FOREIGN KEY weeklyreport_key (weeklyreport_id) REFERENCES weeklyreports (id),
+  FOREIGN KEY member_key (member_id) REFERENCES members (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
