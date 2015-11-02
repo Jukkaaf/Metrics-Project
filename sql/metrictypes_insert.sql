bcef5df7f1cb22813038528826b03e75331d7882
Jukka <Jukka@192.168.1.46>
2015-11-03 00:14:50 +0200

    Added metricstyoe_insert.sql

---
+++ sql/metrictypes_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO metrictypes (id, description) VALUES (0, "phase");
+INSERT INTO metrictypes (id, description) VALUES (1, "reqNew");
+INSERT INTO metrictypes (id, description) VALUES (2, "reqInProgress");
+INSERT INTO metrictypes (id, description) VALUES (3, "reqClosed");
+INSERT INTO metrictypes (id, description) VALUES (4, "reqRejected");
+INSERT INTO metrictypes (id, description) VALUES (5, "commits");
+INSERT INTO metrictypes (id, description) VALUES (6, "passedTestCases");
+INSERT INTO metrictypes (id, description) VALUES (7, "totalTestCases");
