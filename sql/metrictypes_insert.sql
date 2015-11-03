f3054fccb25f6bf243fb040acf1feac7b1dc4680
Jukka <Jukka@192.168.1.46>
2015-11-03 20:00:05 +0200

    Edited metricstyoe_insert.sql to reset the auto_increment before insert

--- sql/metrictypes_insert.sql
+++ sql/metrictypes_insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO metrictypes (id, description) VALUES (0, "phase");
-INSERT INTO metrictypes (id, description) VALUES (1, "reqNew");
-INSERT INTO metrictypes (id, description) VALUES (2, "reqInProgress");
-INSERT INTO metrictypes (id, description) VALUES (3, "reqClosed");
-INSERT INTO metrictypes (id, description) VALUES (4, "reqRejected");
-INSERT INTO metrictypes (id, description) VALUES (5, "commits");
-INSERT INTO metrictypes (id, description) VALUES (6, "passedTestCases");
-INSERT INTO metrictypes (id, description) VALUES (7, "totalTestCases");
+ALTER TABLE metrictypes AUTO_INCREMENT = 1;
+INSERT INTO metrictypes (description) VALUES ("phase");
+INSERT INTO metrictypes (description) VALUES ("reqNew");
+INSERT INTO metrictypes (description) VALUES ("reqInProgress");
+INSERT INTO metrictypes (description) VALUES ("reqClosed");
+INSERT INTO metrictypes (description) VALUES ("reqRejected");
+INSERT INTO metrictypes (description) VALUES ("commits");
+INSERT INTO metrictypes (description) VALUES ("passedTestCases");
+INSERT INTO metrictypes (description) VALUES ("totalTestCases");
